2a454376c55b4bd56278c9639fd4bdeff23ee0b8
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2026-07-13 16:10:40 +0530

    quick script on your database to automatically find any existing tickets

--- app/Filament/Resources/ComplaintRegisterResource/Pages/CreateComplaintRegister.php
+++ app/Filament/Resources/ComplaintRegisterResource/Pages/CreateComplaintRegister.php
@@ -15,6 +15,17 @@ class CreateComplaintRegister extends CreateRecord
         return $this->getResource()::getUrl('index');
     }
 
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['technician_id'])) {
+            $data['status'] = 'Assigned';
+        } else {
+            $data['status'] = 'Open';
+        }
+
+        return $data;
+    }
+
     protected function afterCreate(): void
     {
         $admins = \App\Models\User::whereHas('role', function ($query) {
@@ -24,9 +35,21 @@ class CreateComplaintRegister extends CreateRecord
         foreach ($admins as $admin) {
             \Filament\Notifications\Notification::make()
                 ->title('New Complaint Raised')
-                ->body("A new ticket ({$this->record->ticket_no}) has been raised and is Open.")
+                ->body("A new ticket ({$this->record->ticket_no}) has been raised and is " . $this->record->status . ".")
                 ->warning()
                 ->sendToDatabase($admin);
+        }
+
+        // If a technician was assigned during creation, notify them directly
+        if (!empty($this->record->technician_id)) {
+            $technician = \App\Models\User::find($this->record->technician_id);
+            if ($technician) {
+                \Filament\Notifications\Notification::make()
+                    ->title('Ticket Assigned to You')
+                    ->body("Ticket {$this->record->ticket_no} has been assigned to you upon creation.")
+                    ->success()
+                    ->sendToDatabase($technician);
+            }
         }
     }
 }
