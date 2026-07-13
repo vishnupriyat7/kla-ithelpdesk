94f56a6e5bc435463f34952a1e503022db130ac3
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2026-07-13 12:38:33 +0530

    role-user-seeder-added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Seed roles
+        $roles = [
+            ['id' => 1, 'name' => 'superadmin', 'guard_name' => 'web'],
+            ['id' => 2, 'name' => 'hardwareadmin', 'guard_name' => 'web'],
+            ['id' => 3, 'name' => 'chm', 'guard_name' => 'web'],
+            ['id' => 4, 'name' => 'programmer', 'guard_name' => 'web'],
+            ['id' => 5, 'name' => 'cowd', 'guard_name' => 'web'],
+            ['id' => 6, 'name' => 'admin', 'guard_name' => 'web'],
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($roles as $role) {
+            Role::updateOrCreate(
+                ['id' => $role['id']],
+                ['name' => $role['name'], 'guard_name' => $role['guard_name']]
+            );
+        }
+
+        // Seed superadmin user
+        User::updateOrCreate(
+            ['email' => 'superadmin@kla.com'],
+            [
+                'name' => 'superadmin',
+                'username' => 'superadmin',
+                'password' => Hash::make('superadmin'),
+                'role_id' => 1,
+            ]
+        );
     }
 }
