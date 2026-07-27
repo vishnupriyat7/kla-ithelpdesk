b5d385193dedb349abd1f05b0447703500ff5d94
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2026-07-27 12:52:30 +0530

    Assigned Technician dropdown field for the cowd role-whatsupmessage-stylechange

--- resources/views/complaintregister/live-partial.blade.php
+++ resources/views/complaintregister/live-partial.blade.php
@@ -1175,7 +1175,7 @@
 
 <!-- Status Update Modal -->
 <div class="modal fade" id="statusUpdateModal" tabindex="-1" aria-labelledby="statusUpdateModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="statusUpdateModalLabel">Update Ticket Status</h5>
